603dbd274fcda04dd5fef936236ed9b0caa9be66
AlenaLarzhevska14 <alenna11358@gmail.com>
2026-06-12 23:26:28 +0300

    add authLogout controller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -31,3 +31,18 @@
 // =======================================================================================
 // POST /auth/logout - Вихід користувача із системи.
 // ---------------------------------------------------------------------------------------
+import { Session } from '../models/session.js';
+
+export const logoutUser = async (req, res) => {
+  const { sessionId } = req.cookies;
+
+  if (sessionId) {
+    await Session.deleteOne({ _id: sessionId });
+  }
+
+  res.clearCookie('sessionId');
+  res.clearCookie('accessToken');
+  res.clearCookie('refreshToken');
+
+  res.status(204).send();
+};
