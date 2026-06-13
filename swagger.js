f42173f9f0f35e32b72112885eae67d353ed513d
Dmytro Serhiienko <conneciones@gmail.com>
2026-06-13 22:49:53 +0300

    feat: add swagger documentation and configuration

---
+++ swagger.js
@@ -0,0 +1,19 @@
+import swaggerAutogen from 'swagger-autogen';
+
+const doc = {
+  info: {
+    title: 'Tasteorama API',
+    description: 'API documentation for Tasteorama project',
+    version: '1.0.0',
+  },
+  host: 'localhost:3000',
+  schemes: ['http'],
+};
+
+// Файл, який створиться автоматично (кладемо його в src)
+const outputFile = './src/swagger-output.json';
+
+// Шлях до твого головного файлу сервера, де підключаються всі роути
+const routesEndpointsFiles = ['./src/server.js'];
+
+swaggerAutogen()(outputFile, routesEndpointsFiles, doc);
